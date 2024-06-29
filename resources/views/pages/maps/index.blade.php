aff9b3dbff72574bb316b1d583ab9a7b34614150
silva.zanin@gmail.com <silva.zanin@gmail.com>
2024-06-29 17:47:32 -0300

    Adicionarndo ajustes nas permissões

---
+++ resources/views/pages/maps/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.app')
+
+@section('title', $Page->title)
+
+@section('page_header-title',   $Page->title)
+
+@section('page_content')
+
+    <div class="main-content">
+
+
+    </div><!--/.main-content -->
+
+@endsection
+
+
+@section('script_content')
+
+
+@endsection
